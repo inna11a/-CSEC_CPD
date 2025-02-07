b63a54016b1bdcbd69532e129353633c8ebb28ba
inna11a <gezahegnbashu11a@gmail.com>
2025-02-06 18:40:05 -1200

    Create Petya and strings.cpp

---
+++ Petya and strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin>>a;
+    cin>>b;
+    for(int i=0; i<a.length(); i++){
+        if(tolower(a[i])<tolower(b[i])){
+            cout<<-1;
+            return 0;
+        }
+        else if(tolower(a[i])>tolower(b[i])){
+            cout<<1;
+            return 0;
+        }
+
+    }
+    cout<<0;
+    return 0;
+}
