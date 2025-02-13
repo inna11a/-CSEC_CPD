aaf68092928673b903d0d9def2dcf5ee18376fd0
inna11a <gezahegnbashu11a@gmail.com>
2025-02-12 21:23:34 -1200

    Create Night at the museum .cpp

---
+++ Night at the museum .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int t = 0;
+    char c = 'a';
+    for (char d : s) {
+        int x = abs(d - c);
+        int y = 26 - x;
+        t += min(x, y);
+        c = d;
+    }
+    cout << t << endl;
+    return 0;
+}
+
